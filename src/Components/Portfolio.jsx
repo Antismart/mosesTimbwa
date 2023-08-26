d1f71623b68e849938a935d5622d65a999e537a3
moses timbwa <timbwamoses83@gmail.com>
2023-08-26 17:39:41 +0000

    added my projects

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -45,14 +45,14 @@ const projectList = [
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
+      "Contributed and created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
   {
     title: "GitHub Codespaces and github.dev",
     description:
       "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    // url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
   },
 ];
 
