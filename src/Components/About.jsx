e4ac221291c27f7b946ce2d03a2afeedb72abebe
moses timbwa <timbwamoses83@gmail.com>
2023-08-26 17:22:39 +0000

    Included my skills and info

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm an ICT student studying at Nairobi Technical Training Institute. I take pleasure in crafting distinctive and minimalist user interfaces using my coding skills and innovative approaches as a software developer.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "MERN STACK",
+  "Flutter",
+  "Effective communication with team members and clients",
+  "Kotlin",
+  "Responsive and mobile-first design",
+  "Experience in working in an Agile development environment",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my Software development experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
